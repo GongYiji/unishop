09d03b75f9cfb7b44d060daa5640b53de16af1a5
gongyijie666 <10340347+gongyijie666@user.noreply.gitee.com>
2022-11-04 17:21:25 +0800

    完成了搜索

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,5 +1,8 @@
 <template>
   <view>
+    <!-- 使用自定义的搜索组件 -->
+    <my-search  @click="search"></my-search>
+    
     <view class="scroll-view-container">
       <!-- 左侧滑动区域 -->
       <scroll-view class="left-scroll-view" scroll-y="true" :style="{height: wh+ 'px'}">
@@ -69,13 +72,19 @@
         uni.navigateTo({
           url: '/subpkg/goods_list/goods_list?cid=' + item3.cat_id
         })
+      },
+      search(){
+        console.log(1);
+        uni.navigateTo({
+          url : '/subpkg/search/search'
+        })
       }
     },
       
     onLoad(){
       const sysInfo = uni.getSystemInfoSync ()
       //console.log(sysInfo)
-      this.wh = sysInfo.windowHeight
+      this.wh = sysInfo.windowHeight -50 
       // 调用获取分类列表数据的方法
       this.getCateList()
     }
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,5 +1,9 @@
 <template>
  <view>
+   <!-- 搜索组件 -->
+   <view class="search-box">
+     <my-search @click="search"></my-search>
+   </view>
     <!-- 轮播图区域 -->
         <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
           <!-- 循环渲染轮播图的 item 项 -->
@@ -111,6 +115,12 @@
         })
       
         this.floorList = res.message
+      },
+      search(){
+        console.log(1);
+        uni.navigateTo({
+          url : '/subpkg/search/search'
+        })
       }
       
     }
@@ -153,5 +163,10 @@ swiper {
 .floor-img-box {
   display: flex;
   padding-left: 10rpx;
+}
+.search-box{
+  position: sticky;
+  top: 0;
+  z-index: 999;
 }
 </style>
